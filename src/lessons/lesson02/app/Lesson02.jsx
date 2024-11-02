3c8ee79fb04e92a5842e58bf6d563c6a3b8ce0e0
Vasyl Khvostyk <vasyl.khvostyk@gmail.com>
2024-11-02 09:18:47 +0100

    fix: moved profile data from the lesson02 to var

--- src/lessons/lesson02/app/Lesson02.jsx
+++ src/lessons/lesson02/app/Lesson02.jsx
@@ -1,33 +1,30 @@
 import {ProfileCard} from "../components/ProfileCard";
 
-export const Lesson02 = () => {
+const profileData = {
+    name: 'Mark Zuckerberg',
+    photoUrl: 'https://kress.oberauer-cloud.com/news_detail_slider/rc/L5qBgvCC/uploads/news/Mark_Zuckerberg_Meta.jpg',
+    activities: [
+        'CEO of Meta (formerly Facebook)',
+        'Tech Entrepreneur',
+        'Programmer',
+        'Founder of Meta, Instagram, WhatsApp',
+        'Philanthropist (Chan Zuckerberg Initiative)'
+    ],
+    hobbies: [
+        'Long-distance running',
+        'Learning Mandarin Chinese',
+        'Reading (history, science, biographies)',
+        'Traveling',
+        'Fencing',
+        'Home farming (raising goats)',
+        'Hunting for food',
+        'Brazilian Jiu-Jitsu (BJJ)',
+        'Mixed Martial Arts (MMA)'
+    ]
+}
 
-    return (
-        <div>
-            <ProfileCard
-                name="Mark Zuckerberg"
-                photoUrl="https://kress.oberauer-cloud.com/news_detail_slider/rc/L5qBgvCC/uploads/news/Mark_Zuckerberg_Meta.jpg"
-                activities={[
-                    "CEO of Meta (formerly Facebook)",
-                    "Tech Entrepreneur",
-                    "Programmer",
-                    "Founder of Meta, Instagram, WhatsApp",
-                    "Philanthropist (Chan Zuckerberg Initiative)"
-                ]}
-                hobbies={[
-                    "Long-distance running",
-                    "Learning Mandarin Chinese",
-                    "Reading (history, science, biographies)",
-                    "Traveling",
-                    "Fencing",
-                    "Home farming (raising goats)",
-                    "Hunting for food",
-                    "Brazilian Jiu-Jitsu (BJJ)",
-                    "Mixed Martial Arts (MMA)"
-                ]}
-            />
-        </div>
-    );
+export const Lesson02 = () => {
+    return <ProfileCard {...profileData} />;
 };
 
 export default Lesson02;
