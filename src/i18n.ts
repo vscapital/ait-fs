fe4fedc0b2d43145f4b9423ae5f3249bd2a0ad6d
Vasyl Khvostyk <vasyl.khvostyk@gmail.com>
2024-11-10 12:43:19 +0100

    fix: locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,7 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
-
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-const baseUrl = import.meta.env.PUBLIC_URL || '';
 
 i18n
     .use(Backend)
@@ -12,19 +9,12 @@ i18n
     .use(initReactI18next)
     .init({
         fallbackLng: 'en',
-        debug: false,
+        backend: {
+            loadPath: import.meta.env.VITE_APP_BASE + '/locales/{{lng}}/{{ns}}.json'
+        },
         interpolation: {
             escapeValue: false,
-        },
-        backend: {
-            loadPath: `${baseUrl}/locales/{{lng}}/{{ns}}.json`,
-        },
-        load: 'languageOnly',
-        defaultNS: 'lesson01',
-        react: {
-            useSuspense: false
         }
     });
-
 
 export default i18n;
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
+    }
   }
 })
