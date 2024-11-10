1c46994bbd91f7faa8fcb8b6d8de406f3d9eab47
Vasyl Khvostyk <vasyl.khvostyk@gmail.com>
2024-11-10 13:15:23 +0100

    fix: local

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,6 +4,8 @@ import {initReactI18next} from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const base = import.meta.env.VITE_APP_BASE || '';
+
 i18n
     .use(Backend)
     .use(LanguageDetector)
@@ -15,7 +17,7 @@ i18n
             escapeValue: false,
         },
         backend: {
-            loadPath: '/locales/{{lng}}/{{ns}}.json',
+            loadPath: `${base}/locales/{{lng}}/{{ns}}.json`,
             allowMultiLoading: false,
             crossDomain: false,
         },
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 import i18nextLoader from 'vite-plugin-i18next-loader';
 import path from 'path';
 
+const base = process.env.VITE_APP_BASE || '/';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     i18nextLoader({
       paths: ['./public/locales']
     })],
+  base,
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
